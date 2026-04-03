6007899f92e373bc2d3bb1cc50385ef9b1d7b610
ValeriiKorchahin <valerakorchagin1512@gmail.com>
2026-04-04 00:58:26 +0300

    added db context; registered it for services

--- Corpcore/Program.cs
+++ Corpcore/Program.cs
@@ -1,3 +1,5 @@
+using Corpcore.Database;
+using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +9,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 
 var app = builder.Build();
 
