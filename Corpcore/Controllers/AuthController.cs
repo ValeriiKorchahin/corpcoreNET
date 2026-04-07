4655e18322811fdf5878d032e23c7455282b6923
ValeriiKorchahin <valerakorchagin1512@gmail.com>
2026-04-07 19:01:26 +0300

    added auth controller

---
+++ Corpcore/Controllers/AuthController.cs
@@ -0,0 +1,40 @@
+﻿using Corpcore.Dtos.Auth;
+using Corpcore.Services.Auth;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace Corpcore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController(AuthService _authService) : ControllerBase
+    {
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDto request) 
+        {
+            try
+            {
+                var response = await _authService.Login(request);
+                return Ok(response);
+            }
+            catch
+            {
+                throw new Exception("Failded.");
+            }
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterDto request)
+        {
+            try
+            {
+                var response = await _authService.Register(request);
+                return Ok(response);
+            }
+            catch 
+            {
+                throw new Exception("Register Failed.");
+            }
+    }
+}
